f24feb22b6b37da1271ff48ea67b51bc8ab885e5
Rawat890 <rohanrawat7508@gmail.com>
2026-05-17 18:09:52 +0530

    chore: constants added

---
+++ movie-application/src/utils/constants.ts
@@ -0,0 +1,17 @@
+import { Dimensions } from "react-native";
+
+export const testTitleURL = "https://image.tmdb.org/t/p/w500/nnl6OWkyPpuMm595hmAxNW3rZFn.jpg"
+export const testTitleURL2 = "https://image.tmdb.org/t/p/w500/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg"
+export const testTitleURL3 = "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg"
+
+export const UpcomingMovies = "UpcomingMovies"
+export const PopularMovies = "PopularMovies"
+export const TopRatedMovies = "TopRatedMovies"
+export const TrendingMovies = "TrendingMovies"
+
+const screenHeight = Dimensions.get('window').height;
+const screenWidth = Dimensions.get('window').width;
+
+export { screenHeight, screenWidth };
+export  const DETAIL_VIEW= 'DetailView'; 
+
