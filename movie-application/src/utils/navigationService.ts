6be2c01a59135d08566082341fc4ca309127c2ff
Rawat890 <rohanrawat7508@gmail.com>
2026-05-17 18:14:03 +0530

    chore: navigation service file added and functions added

---
+++ movie-application/src/utils/navigationService.ts
@@ -0,0 +1,33 @@
+import {
+    createNavigationContainerRef,
+    StackActions,
+} from "@react-navigation/native";
+
+export const navigationRef = createNavigationContainerRef();
+
+export function navigate(name: string, params?: object) {
+  if (navigationRef.isReady()) {
+    navigationRef.navigate(name as never, params as never);
+  }
+}
+
+export function goBack() {
+  if (navigationRef.isReady() && navigationRef.canGoBack()) {
+    navigationRef.goBack();
+  }
+}
+
+export function reset(name: string, params?: object) {
+  if (navigationRef.isReady()) {
+    navigationRef.reset({
+      index: 0,
+      routes: [{ name, params }],
+    });
+  }
+}
+
+export function replace(name: string, params?: object) {
+  if (navigationRef.isReady()) {
+    navigationRef.dispatch(StackActions.replace(name, params));
+  }
+}
