b4fd6485adeb93df9e640f2fec6fb7664885ad23
Rawat890 <rohanrawat7508@gmail.com>
2026-05-17 18:13:35 +0530

    chore: SCREENS constant adde and names added of screens

---
+++ movie-application/src/utils/routes.ts
@@ -0,0 +1,8 @@
+export const SCREENS = {
+    Home: "Home",
+    Search: "Search",
+    Download: "Download",
+    Upcoming: "Upcoming",
+    BottomTabs: "BottomTabs",
+    DetailView: "DetailView"
+} as const;
