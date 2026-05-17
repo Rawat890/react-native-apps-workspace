b2ed63b046c0db43a9d8feb885706235ba68f1c4
Rawat890 <rohanrawat7508@gmail.com>
2026-05-17 18:13:49 +0530

    chore: fonts constant added

---
+++ movie-application/src/utils/fonts.ts
@@ -0,0 +1,7 @@
+export const fonts = {
+  notoBold: "NotoSans-Bold",
+  notoExtraBold: "NotoSans-ExtraBold",
+  notoMedium: "NotoSans-Medium",
+  notoRegular: "NotoSans-Regular",
+  notoSemi: "NotoSans-SemiBold",
+};
