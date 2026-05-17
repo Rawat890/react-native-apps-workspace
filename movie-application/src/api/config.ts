19de723878267b6b682cdf1d6e54f53e22e52f17
Rawat890 <rohanrawat7508@gmail.com>
2026-05-17 18:14:33 +0530

    chore: add configuration file for API endpoints and keys

---
+++ movie-application/src/api/config.ts
@@ -0,0 +1,6 @@
+
+export const tmdbBaseURL = "https://api.themoviedb.org/3"
+export const tmdbAccessToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNTBmZDMzMmQxYjcyNzYwYTNmNzJkM2I2YjllZmE2NSIsIm5iZiI6MTc2OTIzNzU4OS4xNjYwMDAxLCJzdWIiOiI2OTc0NmM1NTI0OWYwMzg0MDk2OTNkYTUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.HTUj1LVr6IyeQElXrXkgsv9gAhtN4xCzSF06u1zgHhg"
+export const youtubeKey = "AIzaSyDJNZjCzeOk6WjgjGNbckadqMa2CyWxjCA"; 
+export const youtubaseSearchURl = "https://www.googleapis.com/youtube/v3/search"
+export const watchURL = "https://www.youtube.com/watch"
